0917bbfdd97f8bf3c20ebb04d83cf1eb86d1bfda
EdPaul1 <saidarajab101@gmail.com>
2023-05-15 22:35:46 +0300

    Loops in Ruby

--- looping.rb
+++ looping.rb
@@ -1,6 +1,12 @@
 def happy_new_year
-  # your code here
+  count = 10
+  while count > 0 do
+    puts count
+    count -= 1
+  end
+  puts "Happy New Year!"
 end
+
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
@@ -16,9 +22,17 @@ def fizzbuzz(num)
 end
 
 def fizzbuzz_printer
-  # your code here
+ (1..100).each do |num|
+  puts fizzbuzz(num)
 end
 
 def reverse_string(str)
-  # your code here
+  reversed_str = ""
+  i = str.length - 1
+  while i >= 0 do
+    reversed_str += str[i]
+    i -= 1
+  end
+  return reversed_str
 end
+
